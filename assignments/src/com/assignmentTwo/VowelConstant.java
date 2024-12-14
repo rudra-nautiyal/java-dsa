f1ddf49fd3f2fb93db9c2710ee3a89aeeb0ee2f9
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2024-12-14 23:38:07 +0530

    Vowel or Constant

---
+++ assignments/src/com/assignmentTwo/VowelConstant.java
@@ -0,0 +1,13 @@
+package com.assignmentTwo;
+
+import java.util.Scanner;
+
+public class VowelConstant {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String word = in.next();
+        char letter = word.charAt(0);
+
+    }
+}
