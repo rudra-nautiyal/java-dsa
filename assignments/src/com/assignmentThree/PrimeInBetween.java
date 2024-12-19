db64ab4be9a574e579531c7510891a84157a53ff
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2024-12-19 23:26:17 +0530

    Assignment Three

---
+++ assignments/src/com/assignmentThree/PrimeInBetween.java
@@ -0,0 +1,12 @@
+package com.assignmentThree;
+
+import java.util.Scanner;
+
+public class PrimeInBetween {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int startNumber = in.nextInt();
+        int finalNumber = in.nextInt();
+    }
+}
