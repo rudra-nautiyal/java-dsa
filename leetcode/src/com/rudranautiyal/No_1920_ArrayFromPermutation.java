95b98a7cd7c677764636df57f76f9ec9aeddc281
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2025-01-06 23:28:43 +0530

    No.1920

--- leetcode/src/com/rudranautiyal/No_1920_ArrayFromPermutation.java
+++ leetcode/src/com/rudranautiyal/No_1920_ArrayFromPermutation.java
@@ -1,7 +1,21 @@
 package com.rudranautiyal;
+// https://leetcode.com/problems/build-array-from-permutation/description/
+
+import java.util.Arrays;
 
 public class No_1920_ArrayFromPermutation {
     public static void main(String[] args) {
+        int[] arr = {0,2,1,5,3,4};
 
+        System.out.println(Arrays.toString(buildArray(arr))); // remember Arrays.toString
+    }
+
+    static int[] buildArray(int[] nums) {
+        int[] newarr = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            newarr[i] = nums[nums[i]];
+        }
+
+        return newarr;
     }
 }
