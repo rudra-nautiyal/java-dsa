066ad8ce86e6abec6756ce88ed5c3e2ee5485290
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2025-01-06 23:54:03 +0530

    No. 1929

---
+++ leetcode/src/com/rudranautiyal/No_1929_ConcatenationOfArray.java
@@ -0,0 +1,13 @@
+package com.rudranautiyal;
+// https://leetcode.com/problems/concatenation-of-array/description/
+
+public class No_1929_ConcatenationOfArray {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[2*(nums.length)];
+        for(int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+            ans[nums.length + i] = nums[i];
+        }
+        return ans;
+    }
+}
