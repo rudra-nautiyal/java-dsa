f3d5caa21a1873d32320e12680d2c221f13bb034
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2025-01-09 00:58:57 +0530

    No. 1365

--- leetcode/src/com/rudranautiyal/No_1365_NumberSmallerThanCurrent.java
+++ leetcode/src/com/rudranautiyal/No_1365_NumberSmallerThanCurrent.java
@@ -3,22 +3,18 @@ package com.rudranautiyal;
 public class No_1365_NumberSmallerThanCurrent {
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int[] smallerArray = new int[nums.length];
-
         for(int i = 0; i < nums.length; i++){
             int smaller = 0;
             for (int num : nums) {
                 if (nums[i] == num) {
                     continue;
                 }
-
                 if (num < nums[i]) {
                     smaller++;
                 }
             }
-
             smallerArray[i] = smaller;
         }
-
         return smallerArray;
     }
 }
