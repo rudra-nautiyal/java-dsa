964f519bb000cf7a5f1c943dc26b8f18713dc347
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2025-01-09 17:26:52 +0530

    No. 1832

---
+++ leetcode/src/com/rudranautiyal/No_1832_SentencePangram.java
@@ -0,0 +1,17 @@
+package com.rudranautiyal;
+
+public class No_1832_SentencePangram {
+    public boolean checkIfPangram(String sentence) {
+        if (sentence.length() < 26) {
+            return false;
+        }
+        String alphas = "abcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < alphas.length(); i++) {
+            if (sentence.indexOf(alphas.charAt(i)) == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+}
