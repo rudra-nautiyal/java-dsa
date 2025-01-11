8b85df922848e4eebc5d4f2f7c4601496e32063b
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2025-01-11 16:57:51 +0530

    No. 832

---
+++ leetcode/src/com/rudranautiyal/No_832_FlippingImage.java
@@ -0,0 +1,15 @@
+package com.rudranautiyal;
+
+public class No_832_FlippingImage {
+    public int[][] flipAndInvertImage(int[][] image) {
+        int[][] res = new int[image.length][image[0].length];
+
+        for (int row = 0; row < image.length; row++) {
+            for (int col = 0; col < image[0].length; col++) {
+                res[row][col] = image[row][image[0].length - col - 1] ^ 1;
+            }
+        }
+
+        return res;
+    }
+}
