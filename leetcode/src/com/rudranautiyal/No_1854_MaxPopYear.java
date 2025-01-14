873f2c44924a13371f3d5a4624aa54a362e04e2e
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2025-01-14 18:36:06 +0530

    No. 1854

---
+++ leetcode/src/com/rudranautiyal/No_1854_MaxPopYear.java
@@ -0,0 +1,25 @@
+package com.rudranautiyal;
+
+public class No_1854_MaxPopYear {
+    public int maximumPopulation(int[][] logs) {
+        int maxPopulation = 0;
+        int resultYear = 0;
+
+        for (int year = 1950; year <= 2050; year++) {
+            int count = 0;
+
+            for (int[] log : logs) {
+                if (log[0] <= year && year < log[1]) {
+                    count++;
+                }
+            }
+
+            if (count > maxPopulation) {
+                maxPopulation = count;
+                resultYear = year;
+            }
+        }
+
+        return resultYear;
+    }
+}
