29bfe039acffecd496217678f7e6bcbad8c02b7f
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2025-01-15 20:49:58 +0530

    Day off

--- leetcode/src/com/rudranautiyal/No_1854_MaxPopYear.java
+++ leetcode/src/com/rudranautiyal/No_1854_MaxPopYear.java
@@ -7,19 +7,17 @@ public class No_1854_MaxPopYear {
 
         for (int year = 1950; year <= 2050; year++) {
             int count = 0;
-
             for (int[] log : logs) {
                 if (log[0] <= year && year < log[1]) {
                     count++;
                 }
             }
-
             if (count > maxPopulation) {
                 maxPopulation = count;
                 resultYear = year;
             }
         }
-
+        
         return resultYear;
     }
 }
