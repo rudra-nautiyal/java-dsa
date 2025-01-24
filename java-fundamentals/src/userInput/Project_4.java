8ac917f25d9fda08fb6d9f2dcf42327cc276a286
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2025-01-25 02:06:39 +0530

    Java fundamentals

--- java-fundamentals/src/userInput/Project_4.java
+++ java-fundamentals/src/userInput/Project_4.java
@@ -1,4 +1,12 @@
 package userInput;
 
+import java.util.Scanner;
+
 public class Project_4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        String name = input.nextLine();
+        System.out.println("Good Morning " + name);
+    }
 }
