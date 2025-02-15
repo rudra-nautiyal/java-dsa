5f6f24880669131d6ede1a6f36c40940931b5983
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2025-02-15 23:31:08 +0530

    No. 852

---
+++ leetcode/src/com/rudranautiyal/No_852_PeakOfMountainArray.java
@@ -0,0 +1,20 @@
+package com.rudranautiyal;
+
+public class No_852_PeakOfMountainArray {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start < end){
+            int mid = start + (end - start) / 2;
+
+            if(arr[mid] > arr[mid + 1]){
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return end;
+    }
+}
