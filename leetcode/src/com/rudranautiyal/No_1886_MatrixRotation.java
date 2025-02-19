50c205137e2071c6999476cacea72e0889af0b00
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2025-02-19 15:27:05 +0530

    No. 1886

--- leetcode/src/com/rudranautiyal/No_1886_MatrixRotation.java
+++ leetcode/src/com/rudranautiyal/No_1886_MatrixRotation.java
@@ -1,5 +1,36 @@
 package com.rudranautiyal;
 
 public class No_1886_MatrixRotation {
+    public boolean findRotation(int[][] mat, int[][] target) {
+        for (int i = 0; i < 4; i++) {
+            if(check(mat, target)) {
+                return true;
+            }
 
+            mat = rotate(mat);
+        }
+        return false;
+    }
+
+    int[][] rotate(int[][] arr) {
+        int[][] rotate = new int[arr.length][arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                rotate[j][(arr.length - 1 - i)] = arr[i][j];
+            }
+        }
+
+        return rotate;
+    }
+
+    boolean check(int[][] arr, int[][] target) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (target[i][j] != arr[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
