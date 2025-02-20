432bffeb223d278b8ff803a9d9412fa8d14b8610
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2025-02-21 01:52:00 +0530

    bubble sort

---
+++ sorting/src/BubbleSort.java
@@ -0,0 +1,24 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+
+    }
+
+    static void bubble(int[] arr) {
+        boolean swapped;
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+}
